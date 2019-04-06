3a90d70d683fd50e037cadabf5f5ed06a047824c
gmalho <gaurav.malhotra@nike.com>
2019-04-06 22:59:43 +0200

    first commit

---
+++ bank-balance-exactly-once/src/test/java/com/gonnect/kafka/streams/BankTransactionsProducerTests.java
@@ -0,0 +1,35 @@
+package com.gonnect.kafka.streams;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class BankTransactionsProducerTests {
+
+    @Test
+    public void newRandomTransactionsTest(){
+        ProducerRecord<String, String> record = BankTransactionsProducer.newRandomTransaction("john");
+        String key = record.key();
+        String value = record.value();
+
+        assertEquals("john", key);
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode node = mapper.readTree(value);
+            assertEquals("john", node.get("name").asText());
+            assertTrue("Amount should be less than 100", node.get("amount").asInt() < 100);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(value);
+
+    }
+
+}
